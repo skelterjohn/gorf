5eba87beedcbe4f6301abcca172f819007b740c0
John Asmuth <jasmuth@gmail.com>
2011-03-28 15:45:54 -0400

    moved stuff to readme

--- gorf.go
+++ gorf.go
@@ -1,10 +1,6 @@
 package main
 
 /*
-gorf mvpkg "oldpath" "newpath"
-gorf mvglobal "package path" oldname newname
-  can be for func, type, var, const
-gorf mvfield "package path" typename oldfield newfield
 */
 
 func main() {
